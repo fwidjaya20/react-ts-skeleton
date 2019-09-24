f976e6a1dd7017a4d3890b84fe325aa7ad0ea015
Fredrick Widjaya <f.widjaya20@gmail.com>
2019-09-24 17:15:59 +0700

    implementing webpack

---
+++ apps/web/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import LoginPage from "@skeleton/web/modules/auth/pages/login.page";
+
+ReactDOM.render(
+    <LoginPage />,
+    document.getElementById("root")
+);
---
+++ apps/web/src/modules/auth/pages/login.page.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default class LoginPage extends React.PureComponent<any, any> {
+    render() {
+        return (
+            <div>
+                <h1> Hello Worlds </h1>
+            </div>
+        )
+    }
+};
