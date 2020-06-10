2a663920e8725c379023e1402c0ec121fc00b8b3
Fredrick Widjaya <f.widjaya20@gmail.com>
2020-06-10 11:37:56 +0700

    Add form binding

--- apps/web/src/modules/auth/components/form/login.tsx
+++ apps/web/src/modules/auth/components/form/login.tsx
@@ -1,21 +1,29 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, Ref } from 'react';
 import { Form, Input } from 'antd';
 
-class Login extends Component<any, any> {
+interface LoginFormProps {
+	formRef: Ref<any>;
+}
+
+class Login extends Component<LoginFormProps, any> {
 	render() {
 		return (
 			<Fragment>
-				<Form layout="vertical">
-					<Form.Item>
-						<label> Email </label>
+				<Form ref={this.props.formRef} noValidate layout="vertical">
+					<Form.Item
+						label="Email"
+						name="email"
+						rules={[{ required: true }]}>
 						<Input
 							id="form#email"
 							type="email"
 							placeholder="eg. foo@example.com"
 						/>
 					</Form.Item>
-					<Form.Item>
-						<label> Password </label>
+					<Form.Item
+						label="Password"
+						name="password"
+						rules={[{ required: true }]}>
 						<Input.Password id="form#password" />
 					</Form.Item>
 				</Form>
--- apps/web/src/modules/auth/containers/login.container.tsx
+++ apps/web/src/modules/auth/containers/login.container.tsx
@@ -1,19 +1,39 @@
-import React, { Component, ReactNode, Fragment } from 'react';
-import Login from '@skeleton/web/modules/auth/components/form/login';
+import React, { Component, ReactNode, Fragment, RefObject } from 'react';
 import { Button } from 'antd';
+import Login from '@skeleton/web/modules/auth/components/form/login';
+import { withRouter } from 'react-router';
 
 class LoginFormContainer extends Component<any, any> {
+	private formRef: RefObject<any> = React.createRef<any>();
+
 	render(): ReactNode {
 		return (
 			<Fragment>
-				<Login />
+				<Login formRef={this.formRef} />
 
-				<Button htmlType="button" type="primary" block>
+				<Button
+					block
+					htmlType="button"
+					type="primary"
+					onClick={this.handleOnLogin}>
 					Login
 				</Button>
 			</Fragment>
 		);
 	}
+
+	handleOnLogin = async () => {
+		await this.formRef.current
+			.validateFields()
+			.then((value) => {
+				console.log(value);
+				localStorage.setItem('_token', 'demo');
+				this.props.history.replace('/');
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 }
 
-export default LoginFormContainer;
+export default withRouter(LoginFormContainer);
